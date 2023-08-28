1e3feb27c0fa1761971197be53e47a9507079cfc
Marie-Hélène <mh.roussel@hotmail.fr>
2023-08-28 11:01:33 +0200

    petit exercice

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello Potichat");
+const motApplication = "Pâtépoulet" // Essayez de mettre un autre mot ici !
+let motUtilisateur = prompt("Entrez le mot : " + motApplication)
+
+if (motUtilisateur === motApplication) {
+    console.log("Bravo humain, maintenant tu donne le pâtépoulet!")
+} else {
+    console.log("Tu n'es pas doué humain, maintenant donne quand même le pâtépoulet.")
+}
+
+
