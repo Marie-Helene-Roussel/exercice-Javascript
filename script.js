0855e2f4ca6d7bd286b6340f719c3e1d200b4b12
Marie-Hélène <mh.roussel@hotmail.fr>
2023-09-25 10:36:31 +0200

    avancement

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
 console.log("Hello Potichat");
-const listeMots = ["Pâtépoulet", "Potichats", "Pâtémousse", "Presinours"]
-const listePhrases = ["Dame Bouttiti", "Potichat maitre du monde", "Les potichats sont beau"]
 let score = 0
 
-let choix = prompt("veuillez choisir la liste : mots ou phrases")
-while(choix !== "mots" && choix !== "phrases") {
-	choix = prompt("veuillez choisir la liste : mots ou phrases")
-}
-if (choix === "mots"){
-	
-
- for( let i = 0 ; i < listeMots.length; i++){
-let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
-
-  if (motUtilisateur === listeMots[i]) {
-	score++
-    console.log("Bravo humain, maintenant tu donne le pâtépoulet!")
-  } else {
-
-    console.log("Tu n'es pas doué humain, maintenant donne quand même le pâtépoulet.")
- }	
-
- }
-}
-if (choix === "phrases"){
-	
- for( let i = 0 ; i < listePhrases.length; i++){
-let motUtilisateur = prompt("Entrez la phrase : " + listePhrases[i])
-
-  if (motUtilisateur === listePhrases[i]) {
-	score++
-    console.log("Bravo humain, maintenant tu donne le pâtépoulet!")
-  } else {
-
-    console.log("Tu n'es pas doué humain, maintenant donne quand même le pâtépoulet.")
- }	
-
- }
+function afficherResultat(score, nbMotsProposes) {
+	//affichage du score et résultat du jeu
+	console.log("votre score est de " + score + "sur" + nbMotsProposes)
 }
 
-console.log(score)
+function choisirPhrasesOuMots() {
+	// choix du mode de jeu qui forme une boucle
+	let choix = prompt("veuillez choisir la liste : mots ou phrases")
+	while (choix !== "mots" && choix !== "phrases") {
+		choix = prompt("veuillez choisir la liste : mots ou phrases")
+	}
+	return choix
+}
+function lancerBoucleDeJeu(listePropositions) {
+
+	for (let i = 0; i < listePropositions.length; i++) {
+		motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
+		// calcule du score
+		if (motUtilisateur === listePropositions[i]) {
+			score++
+			console.log("Bravo humain, maintenant tu donne le pâtépoulet!")
+		} else {
+			score--
+			console.log("Tu n'es pas doué humain, maintenant donne quand même le pâtépoulet.")
+		}
+
+	}
+	return score
+
+
+}
+
+function lancerJeu() {
+	let choix = choisirPhrasesOuMots()
+	let score = 0
+	let nbMotsProposes = 0
+
+	if (choix === "mots") {
+		lancerBoucleDeJeu(listeMots)
+		nbMotsProposes = listeMots.length
+	} else {
+		lancerBoucleDeJeu(listePhrases)
+		nbMotsProposes = listePhrases.length
+	}
+
+
+	afficherResultat(score, nbMotsProposes)
+}
+
+/*Lance le jeu de clavier*/
+
+lancerJeu()
