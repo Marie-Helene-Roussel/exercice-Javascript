557a3821de608af315bde408791e87edf42917e6
Marie-Hélène <mh.roussel@hotmail.fr>
2023-11-07 17:54:07 +0100

    rajout d'un mot

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //liste des mots utilisés pour le jeu du clavier potichats.
-const listeMots = ["Pâtépoulet", "Potichats", "Pâtémousse", "Presinours", "Mignon", "Team Jouriste", "Pâtépoisson", "Pâtécrevette"]
+const listeMots = ["Pâtépoulet", "Potichats", "Pâtémousse", "Presinours", "Mignon", "Team Jouriste", "Pâtépoisson", "Pâtécrevette", "PROUT"]
 const listePhrases = ["Dame Bouttiti", "Potichat maitre du monde", "Les potichats sont beau", "Humanis coupablus", "Apaldroit n'existe pas", "c'est mon lelit!"]
 
 
