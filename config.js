3ae65f6a7ad561ca8fbf1084df5d3ad8f0b75ebc
Marie-Hélène <mh.roussel@hotmail.fr>
2023-11-07 15:34:26 +0100

    css fait

--- config.js
+++
@@ -1,5 +0,0 @@
-//liste des mots utilisés pour le jeu du clavier potichats.
-const listeMots = ["Pâtépoulet", "Potichats", "Pâtémousse", "Presinours"]
-const listePhrases = ["Dame Bouttiti", "Potichat maitre du monde", "Les potichats sont beau"]
-
-
