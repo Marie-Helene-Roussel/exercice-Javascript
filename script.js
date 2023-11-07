2a385993375892c4a8c08e120765d6bb5a69c860
Marie-Hélène <mh.roussel@hotmail.fr>
2023-11-07 16:23:37 +0100

    finalisation du CSS

---
+++ script.js
@@ -0,0 +1,11 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les fonctions nécessaires au fonctionnement du jeu. 
+ * 
+ *********************************************************************************/
+
+/**
+ * Cette fonction affiche dans la console le score de l'utilisateur
+ * @param {number} score : le score de l'utilisateur
+ * @param {number} nbMotsProposes : le nombre de mots proposés à l'utilisateur
+ */
