d5f4547aa2b820298049e9d935896ef604ef420e
Marie-Hélène <mh.roussel@hotmail.fr>
2024-10-31 22:51:31 +0100

    j'ai oublié c'est rigolot

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //liste des mots utilisés pour le jeu du clavier potichats.
 const listeMots = ["Pâtépoulet", "Potichats", "Pâtémousse", "Presinours", "Mignon", "Team Jouriste", "Pâtépoisson", "Pâtécrevette", "PROUT"]
-const listePhrases = ["Dame Bouttiti", "Potichat maitre du monde", "Les potichats sont beaux", "Humanis coupablus", "Apaldroit n'existe pas", "c'est mon lelit!", "Qui c'est le plus for entre Nounours et Nougat" ]
+const listePhrases = ["Dame Bouttiti", "Potichat maitre du monde", "Les potichats sont beaux", "Humanis coupablus", "Apaldroit n'existe pas", "c'est mon lelit!", "Qui c'est le plus for entre Nounours et Nougat?" ]
 
 
